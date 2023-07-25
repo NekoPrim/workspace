90e00d8be16cb43150922d9198edbd683a4b009c
Desiree Quade <nekodez@gmail.com>
2023-07-25 10:58:57 -0500

    for loops

--- project1/ForLoops.cpp
+++ project1/ForLoops.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <vector>
 
 using std::cout;
 using std::endl;
+using std::vector;
 
 int main() {
     for (int i {1}; i <= 10; ++ i) {
@@ -25,6 +27,26 @@ int main() {
     // comma operator
     for (int i {1}, j {5}; i <=5; ++ i , ++ j) {
         cout << i << " + " << j << " = " << (i +j) << endl;
+    }
+
+    for (int i {1}; i <= 100; ++i) {
+        // cout << i;
+        // if (i % 10 == 0) {
+        //     cout << endl;
+        // } else {
+        //     cout << " ";
+        // }
+
+        // conditional operator
+        cout << i << (i % 10 == 0 ? "\n" : " ");
+    }
+
+    vector <int> nums {10, 20, 30, 40, 50};
+
+    // make sure comparing same types!
+    // changed int i to unsigned i to match nums.size() type
+    for (unsigned i {0}; i < nums.size(); ++ i) {
+        cout << nums[i] << endl;
     }
 
     cout << endl;
