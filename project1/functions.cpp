7a1ddf3635b78fba605472173d9a87c16c0e0541
Desiree Quade <nekodez@gmail.com>
2023-08-01 18:05:56 -0500

    updated functions

--- project1/functions.cpp
+++ project1/functions.cpp
@@ -5,8 +5,32 @@
 #include <cmath>        // required
 #include <cstdlib>      // required for rand()
 #include <ctime>       // required for time( ) 
+#include <vector>
 
 using namespace std;
+
+void pass_by_value1(int num);
+void pass_by_value2(string s);
+void pass_by_value3(vector<string> v);
+void print_vector(vector<string> v);
+
+void pass_by_value1(int num) {
+    num = 1000;
+}
+
+void pass_by_value2(string s) {
+    s = "Changed";
+}
+
+void pass_by_value3(vector<string> v) {
+    v.clear();  // delete all vector elements
+}
+
+void print_vector(vector<string> v) {
+    for (auto s: v) 
+        cout << s << " ";
+    cout << endl;
+}
 
 const double pi {3.14159};
 
@@ -105,6 +129,33 @@ int main() {
     area_circle();
     area_circle();
     volume_cylinder();
+
+    cout << endl;
+    cout << "\n***********************************" << endl;
+    cout << endl;
+
+    num = 10;
+    int another_num {20};
+
+    cout << "num before calling pass_by_value1: " << num << endl;
+    pass_by_value1(num);
+    cout << "num after calling pass_by_value1: " << num << endl;
+    
+    cout << "\nanother_num before calling pass_by_value1: " << another_num << endl;
+    pass_by_value1(another_num);
+    cout << "another_num after calling pass_by_value1: " << another_num << endl;
+
+    string name {"Frank"};
+    cout << "\nname before calling pass_by_value2: " << name << endl;
+    pass_by_value2(name);
+    cout << "name after calling pass_by_value2: " << name << endl;
+
+    vector<string> stooges {"Larry", "Moe", "Curly"};
+    cout << "\nstooges before calling pass_by_value3: ";
+    print_vector(stooges);
+    pass_by_value3(stooges);
+    cout << "stooges after calling pass_by_value3: ";
+    print_vector(stooges);
 
     cout <<  endl;
     return 0;
